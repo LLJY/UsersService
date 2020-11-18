552cb677d1e497fb13b396441e83fc57cf90a70b
Lucas Lee Jing Yi <lucasleeeeeeeee@gmail.com>
2020-11-18 21:51:18 +0800

    GLOBAL: implement all current functions

---
+++ models/User.ts
@@ -0,0 +1,6 @@
+export type User = {
+    ProfilePictureUri: string,
+    Bio: string,
+    Username: string
+    PhoneNumber: string
+}
--- services/MainService.ts
+++ services/MainService.ts
@@ -1,19 +1,38 @@
 import { handleUnaryCall } from "grpc";
 import { IUserServer } from "../protogen/protos/user_grpc_pb";
 import { GetUserInfoRequest, GetUserInfoResponse, UpdateProfileRequest, UpdateProfileResponse } from "../protogen/protos/user_pb";
+import { collection, add, set, update, get } from 'typesaurus'
+import {User} from "../models/User";
 import * as grpc from "grpc";
 import * as admin from "firebase-admin";
 const app = admin.initializeApp();
 
+const users = collection<User>('users')
 export class MainService implements IUserServer{
     getUserInfo: handleUnaryCall<GetUserInfoRequest, GetUserInfoResponse> = async (call: grpc.ServerUnaryCall<GetUserInfoRequest>, callback: grpc.sendUnaryData<GetUserInfoResponse>)=> {
-        callback(null, new GetUserInfoResponse());
+        // get the user from firestore
+        const user = await get(users, call.request.getUserid());
+        const response = new GetUserInfoResponse();
+        if(user != null){
+            response.setBio(user.data.Bio);
+            response.setPhonenumber(user.data.PhoneNumber);
+            response.setUserid(call.request.getUserid());
+            response.setUsername(user.data.Username);
+            response.setIsexists(true);
+        }else{
+            // if the user does not exist, set it to false so the frontend knows
+            response.setIsexists(false);
+        }
+        callback(null, response);
      }
     updateProfile: handleUnaryCall<UpdateProfileRequest, UpdateProfileResponse> = async (call: grpc.ServerUnaryCall<UpdateProfileRequest>, callback: grpc.sendUnaryData<UpdateProfileResponse>)=>{
-        callback(null, new UpdateProfileResponse)
+        const user = await app.auth().getUser(call.request.getUserid());
+        // set a new record with the user's uid as the document id for easy reference later.
+        await set(users, user.uid, {ProfilePictureUri: call.request.getProfilepictureuri(), Bio: call.request.getBio(), Username: call.request.getUsername(), PhoneNumber: call.request.getPhonenumber()});
+        // set the response fields
+        const response = new UpdateProfileResponse();
+        response.setErrormessage("");
+        response.setIssuccessful(true);
+        callback(null, response);
     }
-    // getUserInfo
-    // updateProfile(call: grpc.ServerUnaryCall<UpdateProfileRequest>, callback: grpc.sendUnaryData<UpdateProfileResponse>): void{
-    //     // TODO
-    // }
 }
